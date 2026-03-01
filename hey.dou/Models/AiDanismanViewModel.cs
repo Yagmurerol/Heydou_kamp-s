28eddf08e06d91a5b50bb2ce9e78ac26eab2954b
aysenurkendirci <aysenurkendirciss@gmail.com>
2026-03-01 16:37:00 +0300

    Al Danışman tool,function calling işlemleri

--- hey.dou/Models/AiDanismanViewModel.cs
+++ hey.dou/Models/AiDanismanViewModel.cs
@@ -5,5 +5,15 @@
         public string Question { get; set; } = string.Empty;
         public string? Answer { get; set; }
         public string? Error { get; set; }
+
+        public List<RelatedPageLink> RelatedPages { get; set; } = new();
+    }
+
+    public class RelatedPageLink
+    {
+        public string Controller { get; set; } = "";
+        public string Action { get; set; } = "Index";
+        public string Label { get; set; } = "";
+        public string Icon { get; set; } = ""; // material-symbols-rounded adı
     }
 }
